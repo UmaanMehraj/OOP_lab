cac3678c5e32a1f03199de85488dd243f7d09ae2
UmaanMehraj <umaanmehraj5@gmail.com>
2025-09-15 15:29:24 +0530

    Fix: printX.cpp

--- printX.cpp
+++ printX.cpp
@@ -7,8 +7,13 @@ int main(){
 
     for (int i = 1; i < rows; i++) {
         for(int j = 0; j < i; j++){
+            if (i & 1)
             cout << "X";
         }
-        cout << "\n";
+        if (i & 1){
+             cout << "\n";
+             rows++;
+
+        }
     }
 }   
