a4f604fe68217a903a5c9db44fe853d07171b429
UmaanMehraj <umaanmehraj5@gmail.com>
2025-12-22 19:04:59 +0530

    Add: q10

--- lafore/objects-classes/q10.cpp
+++ lafore/objects-classes/q10.cpp
@@ -2,14 +2,58 @@
 
 using namespace std;
 
+class Angle{
+    protected:
+        int degrees;
+        float minutes;
+        char direction;
+    public:
+        Angle(): degrees(0), minutes(0), direction('N'){}
+
+        void getData(){
+            cout << "Enter degrees, minutes, direction: ";
+            cin >> degrees >> minutes >> direction;
+
+
+        }
+
+        void showData(){
+            cout <<"Data: "  << degrees << '\xF8' << minutes << '\'' << direction << endl;  
+        }
+
+};
+
+
 class Ship{
     protected:
         static int counter;
+        int number;
+        Angle location;
+    public:
+        Ship(): number(++counter){}
+        void getData(){
+            location.getData();
+        }
+
+        void display(){
+            cout << "Ship Details: " << endl;
+            cout << "Serial is " << number << endl;
+            location.showData();
+        }
         
 
 };
 
-int main(){
+int Ship::counter = 0;
 
+int main(){
+    Ship s1, s2, s3;
+    s1.getData();
+    s2.getData();
+    s3.getData();
+
+    s1.display();
+    s2.display();
+    s3.display();
     return 0;
 }
